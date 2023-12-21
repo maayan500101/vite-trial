e1e25c90cce4bf85edf1b1668c7da55aa0638317
maayan500101 <131789759+maayan500101@users.noreply.github.com>
2023-12-21 19:39:33 +0000

    $

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,75 @@
-function App(){
-  console.log(import.meta.env)
-  const iframeUrl = import.meta.env.VITE_IFRAME_URL  
+function App() {
+  const iframeUrl = import.meta.env.VITE_IFRAME_URL;
 
   const createToken = async (payload, secretKey) => {
     const header = {
-      alg: 'HS256',
-      typ: 'JWT',
+      alg: "HS256",
+      typ: "JWT",
     };
-  
+
     const encodedHeader = btoa(JSON.stringify(header));
     const encodedPayload = btoa(JSON.stringify(payload));
-  
+
     const encodedToken = `${encodedHeader}.${encodedPayload}`;
-  
+
     const secretKeyBuffer = new TextEncoder().encode(secretKey);
     const cryptoKeyPromise = window.crypto.subtle.importKey(
-      'raw',
+      "raw",
       secretKeyBuffer,
-      { name: 'HMAC', hash: { name: 'SHA-256' } },
+      { name: "HMAC", hash: { name: "SHA-256" } },
       false,
-      ['sign']
+      ["sign"]
     );
-  
-  
+
     try {
       const cryptoKey = await cryptoKeyPromise;
       const signatureBuffer = await window.crypto.subtle.sign(
-        { name: 'HMAC', hash: { name: 'SHA-256' } },
+        { name: "HMAC", hash: { name: "SHA-256" } },
         cryptoKey,
         new TextEncoder().encode(encodedToken)
       );
-  
-      const signature = String.fromCharCode.apply(null, new Uint8Array(signatureBuffer));
+
+      const signature = String.fromCharCode.apply(
+        null,
+        new Uint8Array(signatureBuffer)
+      );
       const encodedSignature = btoa(signature);
-  
-      return `${encodedToken}.${encodedSignature}`;
+
+      return `${encodedToken}.${encodedSignature}`
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .slice(0, -1);
     } catch (error) {
-      console.error('Error signing the token:', error);
-      throw error; 
+      console.error("Error signing the token:", error);
+      throw error;
     }
   };
 
+  const loadFunction = async () => {
+    const secret = import.meta.env.VITE_SECRET;
+    const currentHourDate = `${new Date()
+      .toISOString()
+      .split("T")[0]
+      .replaceAll("-", "")}T${new Date().toTimeString().split(":")[0]}`;
 
-  const loadFunction = async() => {
-      const secret =  import.meta.env.VITE_SECRET;
-      const currentHourDate = `${(new Date().toISOString().split("T")[0]).replaceAll("-", "")}T${(new Date()).toTimeString().split(":")[0]}`;
-      const token =  await createToken({
-        "cityId": 107,
-        "journal": "allHospitals",
-        "isAdmin": true
-      } , secret + currentHourDate);
+    const token = await createToken(
+      import.meta.env.VITE_PAYLOAD,
+      secret + currentHourDate
+    );
 
-      console.log(secret + currentHourDate, token);
+    document
+      .querySelector("iframe")
+      .contentWindow.postMessage(token, iframeUrl);
+  };
 
-      document.querySelector("iframe").contentWindow.postMessage(token , iframeUrl);
-  }
-   
-  return  <iframe
-  style={{width:"70vw", height: "90vh"}}
-  id="target_website"
-  src={iframeUrl}
-  onLoad={loadFunction}
->
-</iframe>
+  return (
+    <iframe
+      style={{ width: "70vw", height: "90vh" }}
+      id="target_website"
+      src={iframeUrl}
+      onLoad={loadFunction}
+    ></iframe>
+  );
 }
 
-export default App
+export default App;
