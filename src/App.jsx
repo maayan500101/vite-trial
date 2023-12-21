38264133ab7fc0eca1eb648b6b871e27437fc699
maayan500101 <131789759+maayan500101@users.noreply.github.com>
2023-12-21 16:08:08 +0000

    2

--- src/App.jsx
+++ src/App.jsx
@@ -43,8 +43,8 @@ function App(){
 
   const loadFunction = async() => {
       const secret =  import.meta.env.VITE_SECRET;
+      console.log("joj");
       const currentHourDate = new Date().toISOString().replace(/[-:]/g, '').split('.')[0].replace('T', '');
-      console.log(currentHourDate);
       const token =  await createToken({
         "cityId": 107,
         "journal": "allHospitals",
