dd8c498e3d5b71a3250fc61425b562886a782b2b
maayan500101 <131789759+maayan500101@users.noreply.github.com>
2024-01-27 18:01:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ function App() {
 
   return (
     <iframe
-      style={{ width: "70vw", height: "90vh" }}
+      style={{ width: "75vw", height: "90vh" }}
       id="target_website"
       src={iframeUrl}
       onLoad={loadFunction}
